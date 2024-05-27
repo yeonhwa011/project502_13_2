4fb09089dd8ff62a109ad991ff0a8bb62d1492bd
yeonhwa001 <chyh202@naver.com>
2024-05-27 14:11:13 +0900

    뷰 설계

--- src/main/java/org/choongang/template/main/MainTpl.java
+++ src/main/java/org/choongang/template/main/MainTpl.java
@@ -8,6 +8,10 @@ public class MainTpl implements Template {
     public String getTpl(String tplName) {
         StringBuffer sb = new StringBuffer(2000);
 
+        sb.append("메뉴를 선택하세요.\n")
+                .append("1.회원가입\n")
+                .append("2.로그인\n")
+                .append("-------------------\n");
 
 
         return sb.toString();
