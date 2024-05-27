8ba405f8a9138fd641969656a0ae089482240f98
yeonhwa001 <chyh202@naver.com>
2024-05-27 16:24:13 +0900

    회원가입 기능구현

--- src/main/java/org/choongang/member/controllers/JoinController.java
+++ src/main/java/org/choongang/member/controllers/JoinController.java
@@ -2,19 +2,54 @@ package org.choongang.member.controllers;
 
 import org.choongang.global.AbstractController;
 import org.choongang.global.constants.Menu;
-import org.choongang.template.Template;
+import org.choongang.main.MainRouter;
 import org.choongang.template.Templates;
 
+import java.util.function.Predicate;
+
 /**
- * 회원가입 컨트롤러
+ * 회원 가입 컨트롤러
  *
  */
 public class JoinController extends AbstractController {
-
     @Override
-     public void show(){
-
+    public void show() {
         Templates.getInstance().render(Menu.JOIN);
-      }
     }
 
+    @Override
+    public void prompt() {
+        String userId = promptWithValidation("아이디(6자리 이상): ",
+        s-> s.length() >=6);
+
+        String userPw = promptWithValidation("비밀번호(8자리 이상): ",
+                s->s.length() >=8);
+
+        String confirmPw = promptWithValidation("비밀번호 확인: ",
+                s->{boolean match = s.equals(userPw);
+            if(!match) {
+                System.err.println("\n비밀번호가 일치하지 않습니다.");
+            }
+                return match;
+        });
+
+        String userNm = promptWithValidation("회원명: ",
+                s->!s.isBlank());
+
+        RequestJoin from = RequestJoin.builder()
+                                          .userId(userId)
+                                          .userPw(userPw)
+                                          .confirmPw(confirmPw)
+                                          .userNm(userNm)
+                                          .build();
+
+                //회원가입 처리 ..
+        System.out.println(from);
+
+                //회원가입 성공시 -> 로그인화면 이동
+        MainRouter.getInstance().change(Menu.LOGIN);
+
+    }
+
+
+}
--- src/main/java/org/choongang/member/controllers/LoginController.java
+++ src/main/java/org/choongang/member/controllers/LoginController.java
@@ -2,6 +2,7 @@ package org.choongang.member.controllers;
 
 import org.choongang.global.AbstractController;
 import org.choongang.global.constants.Menu;
+import org.choongang.main.MainRouter;
 import org.choongang.template.Template;
 import org.choongang.template.Templates;
 
@@ -15,5 +16,23 @@ public class LoginController extends AbstractController {
     public void show() {
 
         Templates.getInstance().render(Menu.LOGIN);
+    }
+
+    @Override
+    public void prompt() {
+       String userId = promptWithValidation("아이디: ", s->
+               !s.isBlank());
+       String userPw = promptWithValidation("비밀번호: ",s->
+               !s.isBlank());
+
+       RequestLogin form = RequestLogin.builder()
+               .userId(userId)
+               .userPw(userPw)
+               .build();
+
+        System.out.println(form);
+        //로그인 처리 ...
+        MainRouter.getInstance().change(Menu.MAIN);
+
     }
 }
