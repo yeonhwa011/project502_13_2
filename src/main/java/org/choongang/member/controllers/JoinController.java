de9d449e198619b3ecb1860465b2ec374c513ced
yeonhwa001 <chyh202@naver.com>
2024-05-27 17:04:24 +0900

    회원가입 기능구현

--- src/main/java/org/choongang/member/controllers/JoinController.java
+++ src/main/java/org/choongang/member/controllers/JoinController.java
@@ -22,42 +22,40 @@ public class JoinController extends AbstractController {
 
     @Override
     public void prompt() {
-        String userId = promptWithValidation("아이디(6자리 이상): ",
-        s-> s.length() >=6);
+        String userId = promptWithValidation("아이디(6자리 이상): ", s -> s.length() >= 6);
 
-        String userPw = promptWithValidation("비밀번호(8자리 이상): ",
-                s->s.length() >=8);
+        String userPw = promptWithValidation("비밀번호(8자리 이상): ", s -> s.length() >= 8);
 
-        String confirmPw = promptWithValidation("비밀번호 확인: ",
-                s->{boolean match = s.equals(userPw);
-            if(!match) {
+        String confirmPw = promptWithValidation("비밀번호 확인: ", s -> {
+            boolean match = s.equals(userPw);
+            if (!match) {
                 System.err.println("\n비밀번호가 일치하지 않습니다.");
             }
-                return match;
+
+            return match;
         });
 
-        String userNm = promptWithValidation("회원명: ",
-                s->!s.isBlank());
+        String userNm = promptWithValidation("회원명: ", s -> !s.isBlank());
 
-        RequestJoin from = RequestJoin.builder()
-                                          .userId(userId)
-                                          .userPw(userPw)
-                                          .confirmPw(confirmPw)
-                                          .userNm(userNm)
-                                          .build();
+        RequestJoin form = RequestJoin.builder()
+                .userId(userId)
+                .userPw(userPw)
+                .confirmPw(confirmPw)
+                .userNm(userNm)
+                .build();
 
         Router router = MainRouter.getInstance();
-                //회원가입 처리 ..
-        Service service = MemberServiceLocator.getInstance().find(Menu.JOIN);
-        service.process(form);
+        try {
+            // 회원 가입 처리...
+            Service service = MemberServiceLocator.getInstance().find(Menu.JOIN);
+            service.process(form);
 
-                //회원가입 성공시 -> 로그인화면 이동
-        router.change(Menu.LOGIN);
-    }catch (RuntimeException e){
-        //회원가입 실패시 -> 회원가입 화면으로 이동
-        System.err.println(e.getMessage());
-        router.change(Menu.JOIN);
+            // 회원 가입 성공시 -> 로그인화면 이동
+            router.change(Menu.LOGIN);
+        } catch (RuntimeException e) {
+            // 회원가입 실패시 -> 회원가입 화면으로 이동
+            System.err.println(e.getMessage());
+            router.change(Menu.JOIN);
+        }
     }
-
-
 }
