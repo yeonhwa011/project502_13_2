0c2ebc3d0a07a08b8723efb3e8921c86f445d24b
yeonhwa001 <chyh202@naver.com>
2024-05-27 11:12:53 +0900

    .

---
+++ src/main/java/org/choongang/global/AbstractController.java
@@ -0,0 +1,38 @@
+package org.choongang.global;
+
+import java.util.Scanner;
+
+public abstract class AbstractController implements Controller {
+
+    protected Scanner sc;
+
+    public  AbstractController(){
+        sc = new Scanner(System.in);
+
+    }
+
+    /**
+     * 상단 공통 출력 부분
+     *
+     */
+    public void common(){
+
+    }
+
+    /**
+     * 입력항목
+     */
+    public void prompt(){
+        System.out.print("메뉴선택: ");
+        String menu = sc.nextLine();
+        
+
+    }
+
+    @Override
+    public final void run(){
+        common();
+        show();
+        prompt();
+    }
+}
