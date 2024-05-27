abf40239db4cedd0de47e3112f801eaddb0b5916
yeonhwa001 <chyh202@naver.com>
2024-05-27 16:37:56 +0900

    회원가입 기능구현

---
+++ src/main/java/org/choongang/member/services/MemberServiceLocator.java
@@ -0,0 +1,16 @@
+package org.choongang.member.services;
+
+import org.choongang.global.Service;
+import org.choongang.global.ServiceLocator;
+import org.choongang.global.constants.Menu;
+
+import java.util.PropertyResourceBundle;
+
+public class MemberServiceLocator implements ServiceLocator {
+    private
+
+    @Override
+    public Service find(Menu menu) {
+        return null;
+    }
+}
